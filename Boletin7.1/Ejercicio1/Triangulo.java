94424390675ff6069e844807c90df831cc36a905
Your Name <you@example.com>
2021-01-19 16:34:52 +0100

    Varios ejercicios terminados

--- Boletin7.1/Ejercicio1/Triangulo.java
+++ Boletin7.1/Ejercicio1/Triangulo.java
@@ -4,29 +4,28 @@ public class Triangulo {
 	//Atributos
 	private double base;
 	private double altura;
+	
+	//Métodos construcotores
+		//Constructor por defecto
+		public Triangulo(){
+			base=3;
+			altura=3;
+		}
 
 	//Métodos fundamentales
+	//Getters
 	public double getBase() {
 		return base;
 	}
-
-	public void setBase(double base) {
-		this.base = base;
-	}
-
 	public double getAltura() {
 		return altura;
 	}
-
+	//Setters
+	public void setBase(double base) {
+		this.base = base;
+	}
 	public void setAltura(double altura) {
 		this.altura = altura;
-	}
-	
-	//Métodos construcotores
-	//Constructor por defecto
-	public Triangulo(){
-		base=3;
-		altura=3;
 	}
 
 }
---
+++ Boletin7.1/principal/Main.java
@@ -0,0 +1,15 @@
+package principal;
+
+import Ejercicio3.Persona;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Persona juanjo = new Persona();
+		Persona Antonio = new Persona("Antonio","López",23,'H');
+		juanjo.saludar(Antonio);
+		
+
+	}
+
+}
